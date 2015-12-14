96d9ff6d6e70a09a2c59bc0c5ae3e97159ad19d2
Yeh-Yung <yyc1217@gmail.com>
2015-12-14 20:37:27 +0800

    init

---
+++ src/main/java/com/fake-github/FakeGithubApplication.java
@@ -0,0 +1,12 @@
+package com.fake-github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeGithubApplication.class, args);
+    }
+}
