1f5e6e2cf093ba78b321739cee74e4ed1ac71788
Yeh-Yung <yyc1217@gmail.com>
2015-12-15 20:56:30 +0800

    4 spaces instead of 1 tab

--- src/main/java/com/theservice/service/impl/ClientHookService.java
+++ src/main/java/com/theservice/service/impl/ClientHookService.java
@@ -15,53 +15,57 @@ import com.theservice.service.IClientHookService;
 @Service
 public class ClientHookService implements IClientHookService {
 
-	private static final Logger logger = LoggerFactory.getLogger(ClientHookService.class);
-	
-	@Override
-	public void transferHook(HttpHeaders headers, String body) {
-		
-		String notifyUrl = "http://httpbin.org/post";
-		
-		try {
-			
-			Request request = request(notifyUrl, headers, body);
-						
-			int statusCode = request.execute().returnResponse().getStatusLine().getStatusCode();
-			
-			if (statusCode != 200) {
-				logger.error("{} connection error with status code {}", notifyUrl, statusCode);
-			}
-			
-		} catch (IOException e) {
-			logger.error(notifyUrl + " connection error", e);
-		}
-	}
-	
-	private Request request(String url, HttpHeaders headers, String body) {
-	    
+    private static final Logger logger = LoggerFactory.getLogger(ClientHookService.class);
+
+    @Override
+    public void transferHook(HttpHeaders headers, String body) {
+
+        String notifyUrl = "http://httpbin.org/post";
+
+        try {
+
+            Request request = request(notifyUrl, headers, body);
+
+            int statusCode = request.execute()
+                                    .returnResponse()
+                                    .getStatusLine()
+                                    .getStatusCode();
+
+            if (statusCode != 200) {
+                logger.error("{} connection error with status code {}", notifyUrl, statusCode);
+            }
+
+        } catch (IOException e) {
+            logger.error(notifyUrl + " connection error", e);
+        }
+    }
+
+    private Request request(String url, HttpHeaders headers, String body) {
+
         Request request = Request.Post(url)
                                  .useExpectContinue()
                                  .version(HttpVersion.HTTP_1_1)
                                  .bodyString(body, contentType(headers));
-        
+
         request = copyHeadersToRequest(headers, request);
-        
+
         return request;
-	}
-	
-	private Request copyHeadersToRequest(HttpHeaders headers, Request request) {
+    }
+
+    private Request copyHeadersToRequest(HttpHeaders headers, Request request) {
+        
         headers.remove(HttpHeaders.CONTENT_LENGTH);
-        
+
         headers.toSingleValueMap()
-        .forEach((name, value) -> {
-            request.addHeader(name, value);
-        });
-        
+               .forEach((name, value) -> {
+                   request.addHeader(name, value);
+               });
+
         return request;
     }
 
     private ContentType contentType(HttpHeaders headers) {
-	    return ContentType.create(headers.getContentType().getType());
-	}
+        return ContentType.create(headers.getContentType().getType());
+    }
 
 }
